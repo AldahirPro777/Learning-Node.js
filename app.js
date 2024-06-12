9cef8c5267e35eecb0ec607595c9775dc8b521fd
AldahirPro777 <140104946+AldahirPro777@users.noreply.github.com>
2024-06-12 10:19:45 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+const port = 7777;
+
+//motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// Pagina principal
+app.use(express.static(__dirname + "/public"));
+
+app.use((req,res,next)=>{
+    res.status(404).render("404",{
+        titulo : 404,
+        kindError : "404",
+        descripcion : "Titulo del sitio web"
+    });
+})
+
+app.listen(port, () => {
+  console.log("Hello World al puerto", port);
+});
