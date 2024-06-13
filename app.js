bc8078ebac60faff57488c1be06edf120ec1b75f
AldahirPro777 <1aaldahirmolinamedina@gmail.com>
2024-06-13 13:10:49 -0600

    j

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ const app = express();
 
 const port = 7777;
 
+// conexión a base de datos
+const mongoose = require("mongoose");
+
+const user = "AldaPro";
+const password = "VZdM5x486EDH3qnS";
+const dbname = "veterinaria";
+
+// Asegúrate de que tu URI de conexión es correcta y segura
+const uri = `mongodb+srv://${user}:${password}@cluster0.vvtwvsq.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose
+  .connect(uri, {
+    // Aquí usamos la variable uri
+    // Otras opciones si son necesarias
+  })
+  .then(() => {
+    console.log("Base de datos conectada");
+  })
+  .catch((e) => {
+    console.error("Error al conectar a la base de datos", e);
+  });
+
 // Motor de plantillas
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("mascotas", {
-    arrayMascotas: [
-      { id: "1", nombre: "rex", descripcion: "rex descripcion" },
-      { id: "2", nombre: "chanchan", descripcion: "chanchan descripcion" },
-      { id: "3", nombre: "bella", descripcion: "bella descripcion" },
-      { id: "4", nombre: "toby", descripcion: "toby descripcion" },
-      { id: "5", nombre: "luna", descripcion: "luna descripcion" },
-      { id: "6", nombre: "manchas", descripcion: "manchas descripcion" },
-      { id: "7", nombre: "pelusa", descripcion: "pelusa descripcion" },
-    ],
-  });
+const Mascota = require("../models/mascotas");
+
+router.get("/", async (req, res) => {
+  try {
+    const arrayMascotasDB = await Mascota.find();
+    console.log(arrayMascotasDB);
+
+    res.render("mascotas", {
+      arrayMascotas: arrayMascotasDB,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
