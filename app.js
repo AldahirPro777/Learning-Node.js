23453f8e1e58ceec419db29523ffb2195f6a721c
AldahirPro777 <1aaldahirmolinamedina@gmail.com>
2024-06-13 08:53:52 -0600

    fi

--- app.js
+++ app.js
@@ -4,22 +4,24 @@ const app = express();
 
 const port = 7777;
 
-//motor de plantillas
+// Motor de plantillas
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 // Pagina principal
 app.use(express.static(__dirname + "/public"));
 
-app.use('/',require('./router/routersWeb'));
+// Rutas web
+app.use("/", require("./router/routersWeb"));
+app.use("/mascotas", require("./router/Mascotas"));
 
-app.use((req,res,next)=>{
-    res.status(404).render("404",{
-        titulo : 404,
-        kindError : "404",
-        descripcion : "Titulo del sitio web"
-    });
-})
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+    titulo: 404,
+    kindError: "404",
+    descripcion: "Titulo del sitio web",
+  });
+});
 
 app.listen(port, () => {
   console.log("Hello World al puerto", port);
--- router/routersWeb.js
+++ router/routersWeb.js
@@ -1,17 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index", {
-        titulo : "Main"
-    });
+  res.render("index", {
+    titulo: "Main",
+  });
 });
 
 router.get("/servicios", (req, res) => {
-    res.render("servicios", {
-        titulo : "Servicios",
-        tituloServicios : "Este es un mensaje dinamico de servicios"
-    });
+  res.render("servicios", {
+    titulo: "Servicios",
+    tituloServicios: "Este es un mensaje dinamico de servicios",
+  });
 });
+
+/* 
+router.get("/mascotas", (req, res) => {
+    res.render("servicios", {
+        titulo : "Mascotas"
+    });
+}); */
 
 module.exports = router;
